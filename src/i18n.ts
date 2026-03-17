3654e492d0a711607b6463a7d2404356a9cdd850
laura-0210 <laudelpozo.26@gmail.com>
2026-03-17 14:08:00 +0100

    añadidos frances, aleman y portugues y corregido en el i18n.ts

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,6 +4,9 @@ import LanguageDetector from "i18next-browser-languagedetector";
 
 import es from "./locales/es.json";
 import en from "./locales/en.json";
+import fr from "./locales/fr.json";
+import de from "./locales/de.json";
+import pt from "./locales/pt.json";
 
 i18n
   .use(LanguageDetector)
@@ -12,6 +15,9 @@ i18n
     resources: {
       es: { translation: es },
       en: { translation: en },
+      fr: { translation: fr },
+      de: { translation: de },
+      pt: { translation: pt },
     },
     fallbackLng: "es",
     interpolation: {
