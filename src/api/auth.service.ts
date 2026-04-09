6c6dbcdd7c8bbace9e310d997f9748e8b6b7f326
laura-0210 <laudelpozo.26@gmail.com>
2026-04-09 16:47:31 +0200

    cambios para api

--- src/api/auth.service.ts
+++ src/api/auth.service.ts
@@ -1,20 +1,30 @@
-
 import { api, saveToken, clearToken } from "./axiosInstance";
 import type { TokenResponse } from "./api.types";
- 
+
 export async function login(
   username: string,
   password: string,
-  persistent = false
+  persistent = false,
 ): Promise<void> {
   const { data } = await api.post<TokenResponse>(
     "/auth/token",
     new URLSearchParams({ username, password }),
-    { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    { headers: { "Content-Type": "application/x-www-form-urlencoded" } },
   );
   saveToken(data.access_token, persistent);
 }
- 
+
 export function logout(): void {
   clearToken();
+}
+export async function loginGuest(
+  bookingId: string,
+  surname: string,
+): Promise<void> {
+  const { data } = await api.post<TokenResponse>("/auth/guest-login", {
+    booking_id: bookingId,
+    surname,
+  });
+
+  saveToken(data.access_token, false);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,12 +11,20 @@ export default defineConfig({
       //   import { Button } from '@/components/ui'
       //   import { useCheckin } from '@/hooks/useCheckin'
       //   import type { StepId } from '@/types'
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-    server: {
+  server: {
     host: true,
     port: 5173,
     allowedHosts: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        // Esto borra "/api-tutor" antes de enviárselo a FastAPI
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
